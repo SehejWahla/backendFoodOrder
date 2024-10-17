046e5cf813c126bbbf1be0af81aa944365dacec7
Sehej Wahla <sehejwahla@gmail.com>
2024-10-18 01:31:32 +1100

    started working on authentification

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,19 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import pg from 'pg'
-const { Pool } = pg
- 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,     
-  password: process.env.DB_PASSWORD
-})
+import pg from "pg";
+import { jwtDecode } from "jwt-decode";
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
+  console.log(jwtDecode(req.body.credentials));
+  res.status(200).send({ success: "ok" });
+});
 
-
-
-
-app.get('/', async (req:Request, res:Response)=>{
-    res.status(200).send({"success" : "ok"})
-})
-
-app.listen(3000, async ()=>{
-    console.log('server started on localhost:3000')
-})
+app.listen(3000, async () => {
+  console.log("server started on localhost:3000");
+});
